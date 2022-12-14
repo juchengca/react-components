e807340ff6614de9d32bf332fbb025116d2eda2e
Justin Cheng <jucheng.ca@gmail.com>
2022-12-14 15:57:28 -0800

    Completed React Components

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // TODO
+
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+
+  const [isDone, setIsDone] = useState(false);
+
+  const style = {
+    //textDecoration: isDone ? 'line-through' : 'none',
+    fontWeight: isDone ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onMouseEnter={() => setIsDone(!isDone)}>
+      {props.item}
+    </li>
+  );
+
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Lemons', 'Potatoes', 'Soda']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
